c78f644f057d9451606fe6d8d36824e857f4e36a
foorcun <msnifoorcun@hotmail.com>
2023-03-28 00:41:10 +0300

    readme update

---
+++ MyModules/FlutterStart/src/features/FlutterStartHome/components/LeanAppBar.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { AppBar } from "@react-native-material/core";
+
+const LeanAppBar = () => <AppBar title="React Native Demo Home Page" />;
+
+export default LeanAppBar;
--- MyModules/FlutterStart/src/features/FlutterStartHome/screens/FlutterStart.js
+++ MyModules/FlutterStart/src/features/FlutterStartHome/screens/FlutterStart.js
@@ -1,15 +1,21 @@
 import { width } from "@mui/system";
 import { View, Text, StyleSheet, Dimensions } from "react-native";
 import ButtonAppBar from "../components/ButtonAppBar";
+import FloatingButton from "../components/FloatingButton";
+import LeanAppBar from "../components/LeanAppBar";
 // import { Dimensions } from "react-native";
 
 export default function FlutterStart() {
   return (
     <View style={{ flex: 1, backgroundColor: "pink" }}>
-      <ButtonAppBar></ButtonAppBar>
+      {/* <ButtonAppBar></ButtonAppBar> */}
+      <LeanAppBar />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text style={styles.text}>Hello React</Text>
+        <Text style={styles.text}>
+          You have pushed the button this many times:
+        </Text>
       </View>
+      <FloatingButton></FloatingButton>
     </View>
   );
 }
